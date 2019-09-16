3bff622dd053ae49dd57c80ac50fda9eb96eb624
tangjiujun <jiujun.tang@thoughtworks.com>
2019-09-16 16:36:57 +0800

    when parking lot is full throw exception

--- src/main/java/com/tw/parkinglot/ParkingLot.java
+++ src/main/java/com/tw/parkinglot/ParkingLot.java
@@ -1,5 +1,6 @@
 package com.tw.parkinglot;
 
+import java.util.EmptyStackException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -15,7 +16,12 @@ public class ParkingLot {
     }
 
     public int parking(String carNo) {
-        Integer slotNo = availableSlots.pop();
+        Integer slotNo;
+        try {
+            slotNo = availableSlots.pop();
+        } catch (EmptyStackException e) {
+            throw new IllegalStateException("Parking Lot is Full.");
+        }
         parkingSlots.put(slotNo, carNo);
         return slotNo;
     }
